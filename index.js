826ba4b7053b5ddfe8decdfb5bcd43fe722b6af0
Wen Bao Yin <supermq@gmail.com>
2020-11-12 10:27:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const route = require('koa-route');
 const axios = require('axios');
 const qs = require('qs');
 
+const OAUTH_GRANT_TYPES = {
+  code: 'authorization_code',
+  refresh: 'refresh_token',
+};
+
 const oAuthConfig = {
   clientID: 'node-client-example',
   clientSecret: '37KXvZxQcws~DLgrV51kNHHcbi',
@@ -12,7 +17,7 @@ const oAuthConfig = {
   tokenURI: 'http://127.0.0.1:4444/oauth2/token',
   callbackURI: 'http://127.0.0.1:8080/oauth/callback',
   introspectURI: 'http://127.0.0.1:4445/oauth2/introspect',
-  scopes: ['openid', 'offline', 'offline_access'],
+  scopes: ['openid', 'offline_access'],
 };
 
 const app = new Koa();
@@ -33,31 +38,29 @@ const callback = async ctx => {
   const { code, state } = ctx.request.query;
   const { callbackURI, clientID, clientSecret } = oAuthConfig;
   console.log('authorization code:', code);
-  let tokenResponse = {};
+  let tokenInfo = {};
   try {
-    const data = qs.stringify({
-      grant_type: 'authorization_code',
-      client_id: clientID,
-      client_secret: clientSecret,
-      redirect_uri: callbackURI,
-      code,
-      state,
-    });
-
-    tokenResponse = await axios.post(oAuthConfig.tokenURI, data, {
-      headers: {
-        accept: 'application/json',
-      },
-    });
+    tokenInfo = await getToken({ code, state });
   } catch (e) {
     console.log(e);
   }
 
-  const accessToken = tokenResponse.data.access_token;
-  console.log(`access token: ${accessToken}`);
+  console.log(tokenInfo);
 
-  const { sub: userName } = await introspect(accessToken);
-  ctx.response.redirect(`/welcome.html?name=${userName}`);
+  try {
+    tokenInfo = await getToken(
+      { state, refresh_token: tokenInfo.refresh_token },
+      OAUTH_GRANT_TYPES.refresh,
+    );
+  } catch (e) {
+    console.log(e);
+  }
+
+  const userInfo = await introspect(tokenInfo.access_token);
+  ctx.body = `<ul>
+    <li>tokenInfo: <pre>${JSON.stringify(tokenInfo, null, 4)}</pre></li>
+    <li>userinfo: <pre>${JSON.stringify(userInfo, null, 4)}</pre></li>
+  </ul>`;
 };
 
 const introspect = async accessToken => {
@@ -75,6 +78,32 @@ const introspect = async accessToken => {
     console.log(e);
   }
   return result.data;
+};
+
+/* retrieve token by code or refresh_token. */
+const getToken = async (params, grantType = OAUTH_GRANT_TYPES.code) => {
+  const { callbackURI, clientID, clientSecret, tokenURI } = oAuthConfig;
+  const authHeader = `Basic ${Buffer.from(`${clientID}:${clientSecret}`, 'binary').toString(
+    'base64',
+  )}`;
+  const data = qs.stringify({
+    grant_type: grantType,
+    redirect_uri: callbackURI,
+    ...params,
+  });
+  let tokenResponse = {};
+  try {
+    tokenResponse = await axios.post(tokenURI, data, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: authHeader,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    debugger;
+  }
+  return tokenResponse.data;
 };
 
 app.use(main);
